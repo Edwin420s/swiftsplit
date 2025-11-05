da4657d14120584de75cf6c40d7bdf7b2b082895
Edwin Mwiti <eduedwyn5@gmail.com>
2025-11-05 07:34:24 +0300

    Initial commit

---
+++ backend/src/middleware/socketAuth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const socketAuth = async (socket, next) => {
+  try {
+    const token = socket.handshake.auth.token;
+    
+    if (!token) {
+      return next(new Error('Authentication error: No token provided'));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findByPk(decoded.userId);
+    
+    if (!user) {
+      return next(new Error('Authentication error: User not found'));
+    }
+
+    socket.userId = user.id;
+    socket.user = user;
+    next();
+  } catch (error) {
+    next(new Error('Authentication error: ' + error.message));
+  }
+};
+
+module.exports = socketAuth;
