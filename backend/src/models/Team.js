cc141e6a0915a0ad0f00f7683af90122ff569194
Edwin Mwiti <eduedwyn5@gmail.com>
2025-11-05 07:48:33 +0300

    Initial commit

--- backend/src/models/Team.js
+++ backend/src/models/Team.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const User = require('./User');
 
 const Team = sequelize.define('Team', {
   id: {
@@ -9,7 +10,10 @@ const Team = sequelize.define('Team', {
   },
   name: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   description: {
     type: DataTypes.TEXT
@@ -17,7 +21,18 @@ const Team = sequelize.define('Team', {
   defaultSplitType: {
     type: DataTypes.ENUM('equal', 'percentage', 'fixed'),
     defaultValue: 'equal'
+  },
+  createdBy: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id'
+    }
   }
+}, {
+  tableName: 'teams',
+  timestamps: true
 });
 
 const TeamMember = sequelize.define('TeamMember', {
@@ -26,9 +41,29 @@ const TeamMember = sequelize.define('TeamMember', {
     defaultValue: DataTypes.UUIDV4,
     primaryKey: true
   },
+  teamId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'Teams',
+      key: 'id'
+    }
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id'
+    }
+  },
   splitPercentage: {
     type: DataTypes.DECIMAL(5, 2),
-    defaultValue: 0
+    defaultValue: 0,
+    validate: {
+      min: 0,
+      max: 100
+    }
   },
   fixedAmount: {
     type: DataTypes.DECIMAL(12, 2),
@@ -38,11 +73,73 @@ const TeamMember = sequelize.define('TeamMember', {
     type: DataTypes.BOOLEAN,
     defaultValue: true
   }
+}, {
+  tableName: 'team_members',
+  timestamps: true
 });
 
 // Associations
-Team.hasMany(TeamMember, { foreignKey: 'teamId' });
+Team.belongsTo(User, { foreignKey: 'createdBy', as: 'creator' });
+Team.hasMany(TeamMember, { foreignKey: 'teamId', as: 'members' });
 TeamMember.belongsTo(Team, { foreignKey: 'teamId' });
-TeamMember.belongsTo(require('./User'), { foreignKey: 'userId' });
+TeamMember.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(TeamMember, { foreignKey: 'userId' });
+
+// Instance methods
+Team.prototype.getActiveMembers = async function() {
+  return await TeamMember.findAll({
+    where: { 
+      teamId: this.id,
+      isActive: true 
+    },
+    include: [User]
+  });
+};
+
+Team.prototype.calculateSplits = function(totalAmount) {
+  const members = this.members || [];
+  const activeMembers = members.filter(m => m.isActive);
+  
+  switch (this.defaultSplitType) {
+    case 'equal':
+      const equalAmount = totalAmount / activeMembers.length;
+      return activeMembers.map(member => ({
+        userId: member.userId,
+        walletAddress: member.User.walletAddress,
+        amount: equalAmount,
+        percentage: 100 / activeMembers.length
+      }));
+
+    case 'percentage':
+      return activeMembers.map(member => ({
+        userId: member.userId,
+        walletAddress: member.User.walletAddress,
+        amount: totalAmount * (member.splitPercentage / 100),
+        percentage: member.splitPercentage
+      }));
+
+    case 'fixed':
+      return activeMembers.map(member => ({
+        userId: member.userId,
+        walletAddress: member.User.walletAddress,
+        amount: member.fixedAmount,
+        percentage: (member.fixedAmount / totalAmount) * 100
+      }));
+
+    default:
+      throw new Error('Invalid split type');
+  }
+};
+
+// Static methods
+Team.getUserTeams = async function(userId) {
+  return await Team.findAll({
+    include: [{
+      model: TeamMember,
+      where: { userId },
+      required: true
+    }]
+  });
+};
 
 module.exports = { Team, TeamMember };
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
   id: {
@@ -10,29 +11,88 @@ const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [6, 100]
+    }
   },
   name: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  role: {
+    type: DataTypes.ENUM('client', 'freelancer', 'team_manager'),
+    allowNull: false,
+    defaultValue: 'freelancer'
   },
   walletAddress: {
     type: DataTypes.STRING,
     unique: true,
-    allowNull: false
+    allowNull: true // Initially null until wallet is created
   },
-  role: {
-    type: DataTypes.ENUM('client', 'freelancer', 'team_manager'),
-    allowNull: false
+  circleUserId: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: true
+  },
+  walletId: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: true
   },
   kycStatus: {
     type: DataTypes.BOOLEAN,
     defaultValue: false
   },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 12);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        user.password = await bcrypt.hash(user.password, 12);
+      }
+    }
   }
 });
+
+// Instance methods
+User.prototype.validatePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+User.prototype.toJSON = function() {
+  const values = { ...this.get() };
+  delete values.password;
+  return values;
+};
+
+// Static methods
+User.findByEmail = async function(email) {
+  return await this.findOne({ where: { email } });
+};
+
+User.findByWalletAddress = async function(walletAddress) {
+  return await this.findOne({ where: { walletAddress } });
+};
 
 module.exports = User;
